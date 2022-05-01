5e0180832a8057a92d59975e30406478067009dc
interesting1113 <sylviayang1113@gmail.com>
2022-05-01 13:00:48 +0900

    热门搜索样式布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -8,10 +8,39 @@ import {
   Nav,
   NavItem,
   SearchWrapper,
+  SearchInfo,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoList,
+  SearchInfoItem,
   NavSearch,
   Addition,
-  Button,
+  Button
 } from './style';
+
+const getListArea = (show) => {
+  if (show) {
+    return (
+      <SearchInfo>
+        <SearchInfoTitle>
+          热门搜索
+          <SearchInfoSwitch>换一批</SearchInfoSwitch>
+        </SearchInfoTitle>
+        <SearchInfoList>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+          <SearchInfoItem></SearchInfoItem>
+        </SearchInfoList>
+      </SearchInfo>
+    )
+  } else {
+    return null;
+  }
+}
 
 const Header = (props) => {
   return (
@@ -39,6 +68,7 @@ const Header = (props) => {
             <i className={props.focused ? 'focused iconfont': 'iconfont'}>
               &#xe614;
             </i>
+            {getListArea(props.focused)}
           </SearchWrapper>
         </Nav>
         <Addition>
