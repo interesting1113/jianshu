67e02b3a94192b9e3ec7f6e27e944a9251e66a89
interesting1113 <sylviayang1113@gmail.com>
2022-05-07 14:25:06 +0900

    首页异步数据获取

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,13 +1,19 @@
 import { fromJS, set } from 'immutable';
 
 const defaultState = fromJS({
-  topicList: [{
-    id: 1
-  }]
+  topicList: [],
+  articleList: [],
+  recommendList: []
 });
 
 export default (state = defaultState, action) => {
   switch(action.type) {
+    case 'change_home_data':
+      return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList)
+      })
     default:
       return state;
   }
