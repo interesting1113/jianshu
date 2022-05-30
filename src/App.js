ecd88fcce85f7519bb19028246a6cd257e0859e9
interesting1113 <sylviayang1113@gmail.com>
2022-05-30 18:42:06 +0900

    登陆页面布局

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowerRouter, Route} from 'react-router-dom';
 import Header from "./common/header";
 import Home from './pages/home';
 import Detail from './pages/detail';
+import Login from './pages/login';
 import store from './store/index'
 
 class App extends Component {
@@ -14,8 +15,9 @@ class App extends Component {
             <BrowerRouter>
               <div>
                 <Header/>
-                <Route path='/' exact component={Home}>home</Route>
-                <Route path='/detail/:id' exact component={Detail}>detail</Route>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/login' exact component={Login}></Route>
+                <Route path='/detail/:id' exact component={Detail}></Route>
               </div>
             </BrowerRouter>
         </Provider>
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { Content, DetailWrapper, Header} from './style';
 import { actionCreators } from "./store";
 
-class Detail extends Component {
+class Detail extends PureComponent {
   render () {
     return (
       <DetailWrapper>
